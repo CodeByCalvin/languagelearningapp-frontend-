d83fa5dc2013162cdba1d4aa887de70e136bdd78
CodeByCalvin <calvin@calvinmerry.com>
2023-07-04 11:26:38 +0100

    Commit

--- languagelearningapp/src/components/home.jsx
+++ languagelearningapp/src/components/home.jsx
@@ -6,32 +6,51 @@ import { ReactComponent as CircleCheckIcon } from "../imgs/icons/circle-check-so
 import { ReactComponent as ProgressIcon } from "../imgs/icons/bars-progress-solid.svg";
 import { ReactComponent as CogWheelIcon } from "../imgs/icons/gear-solid.svg";
 import Navbar from "../components/navbar";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const buttons = [
+    { Icon: CalendarCheckIcon, text: "Word of the Day" },
+    { Icon: GraduationCapIcon, text: "Learn" },
+    { Icon: CircleCheckIcon, text: "Review" },
+    { Icon: ProgressIcon, text: "Progress" },
+    { Icon: CogWheelIcon, text: "Settings" },
+  ];
+
+  const buttonVariants = {
+    hidden: { opacity: 0, y: 30 },
+    visible: (index) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: index * 0.07,
+      },
+    }),
+    whileTap: { scale: 0.9 },
+  };
+
   return (
     <div className="main-content">
-      <div className="buttons-container">
-        <button className="home-btn">
-          <CalendarCheckIcon className="home-btn-icon" />
-          Word of the Day
-        </button>
-        <button className="home-btn">
-          <GraduationCapIcon className="home-btn-icon" />
-          Learn
-        </button>
-        <button className="home-btn">
-          <CircleCheckIcon className="home-btn-icon" />
-          Review
-        </button>
-        <button className="home-btn">
-          <ProgressIcon className="home-btn-icon" />
-          Progress
-        </button>
-        <button className="home-btn">
-          <CogWheelIcon className="home-btn-icon" />
-          Settings
-        </button>
-      </div>
+      <motion.div
+        className="buttons-container"
+        initial="hidden"
+        animate="visible"
+      >
+        {buttons.map((button, index) => (
+          <motion.button
+            key={index}
+            className="home-btn"
+            custom={index}
+            variants={buttonVariants}
+            initial="hidden"
+            animate="visible"
+            whileTap="whileTap"
+          >
+            <button.Icon className="home-btn-icon" />
+            {button.text}
+          </motion.button>
+        ))}
+      </motion.div>
       <Navbar />
     </div>
   );
