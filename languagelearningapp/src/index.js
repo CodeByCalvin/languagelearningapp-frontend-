a7cf607d36c71b1a95e4a81027c69d3c0cc73d71
CodeByCalvin <calvin@calvinmerry.com>
2023-07-12 10:20:32 +0100

    Updated index.js

--- languagelearningapp/src/index.js
+++ languagelearningapp/src/index.js
@@ -7,11 +7,12 @@ import Home from "./components/home";
 import { Login } from "./components/login";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Game from "./components/game";
+import login from "./components/login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Game />
+    <App />
   </React.StrictMode>
 );
 
